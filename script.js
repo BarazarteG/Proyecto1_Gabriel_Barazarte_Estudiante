807eda249c7a4b942ce60940571b4cd387ea3a36
BarazarteG <gabrielbarazarte293@gmail.com>
2024-10-19 10:44:23 -0500

    Par_o_Impar

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let iterador = 1; iterador <= 20; iterador++){
+
+    if (iterador%2 == 0){
+        console.log(`${iterador} es par`);
+        }else{
+            console.log(`${iterador} es impar`);        
+        }
+}
