2c75734bffd90c3abc50fe7f6f8cb2401440e66b
BarazarteG <gabrielbarazarte293@gmail.com>
2024-10-19 11:34:06 -0500

    par-o-impar

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-
-let bton = document.getElementById("btn");
-let arrNum = [20]
-let mensaje = document.createElement("p");
-let pe = document.getElementById('texto');
-bton.addEventListener('click', (e) => {
-    for(let cont = 0; cont <= 20; cont++){
-        arrNum[cont] = cont;
-        if (arrNum[cont]%2 == 0){
-            let messenger = document.createTextNode(`${arrNum[cont]} es par `);
-            mensaje.appendChild(messenger);
-            document.body.insertBefore(mensaje, pe);
-        } else{
-            let messenger = document.createTextNode(`${arrNum[cont]} es par `);
-            mensaje.appendChild(messenger);
-            document.body.insertBefore(mensaje, pe);
-        }
+console.log('hola mundo');
+for (let index = 0; index <=20; index++) {
+    if (index % 2 == 0){
+        console.log(`${index} es par `);
+    } else {
+        console.log(`${index} es impar `);
     }
-});
-
+}
